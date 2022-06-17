8eaee0e92b031d6f6a56750c933b4c3af8ff376f
surya-aji <aedjiae1@gmail.com>
2022-06-18 02:11:41 +0700

    Create Mahasiswa

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// library
+var flash = require('express-flash');
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mahasiswaRouter = require('./routes/mahasiswa');
@@ -15,12 +19,22 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({ 
+  cookie: { 
+  maxAge: 60000 
+  },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+  }))
+  
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -11,15 +11,144 @@ router.get('/',function(req,res,next){
         if(err){
             req.flash('error',err);
             res.render('pages/tugas',{
-                data:''
+                data:'',
+                nama: '',
+                nim: '',
+                alamat: '',
+                email: '',
+                prodi: '',
             });
         }else{
             // render ke index mahasiswa
             res.render('pages/tugas',{
-                data: rows
+                data: rows,
+                nama: '',
+                nim: '',
+                alamat: '',
+                email: '',
+                prodi: '',
             });
         }
     })
 });
+
+// Create Mahasiswa
+// router.get('/create',function(req, res, next){
+//     res.render('pages/add-tugas'),{
+//         nama: '',
+        // nim: '',
+        // alamat: '',
+        // email: '',
+        // prodi: '',
+//     }
+// });
+
+// Store Mahasiswa
+router.post('/store',function(req, res, next){
+    let nama = req.body.nama;
+    let nim = req.body.nim;
+    let alamat = req.body.alamat;
+    let email = req.body.email;
+    let prodi = req.body.prodi;
+    let errors = false;
+
+    if(nama.length === 0) {
+        errors = true;
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Nama");
+        // render to add.ejs with flash message
+        res.render('pages/tugas', {
+        nama: nama,
+        nim: nim,
+        alamat: alamat,
+        email: email,
+        prodi: prodi,
+        })
+    }
+
+    if(nim.length === 0) {
+        errors = true;
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Nim");
+        // render to add.ejs with flash message
+        res.render('pages/tugas', {
+        nama: nama,
+        nim: nim,
+        alamat: alamat,
+        email: email,
+        prodi: prodi,
+        })
+    }
+    if(alamat.length === 0) {
+        errors = true;
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Alamat");
+        // render to add.ejs with flash message
+        res.render('pages/tugas', {
+        nama: nama,
+        nim: nim,
+        alamat: alamat,
+        email: email,
+        prodi: prodi,
+        })
+    }
+    if(email.length === 0) {
+        errors = true;
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Email");
+        // render to add.ejs with flash message
+        res.render('pages/tugas', {
+        nama: nama,
+        nim: nim,
+        alamat: alamat,
+        email: email,
+        prodi: prodi,
+        })
+    }
+    if(prodi.length === 0) {
+        errors = true;
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Prodi");
+        // render to add.ejs with flash message
+        res.render('pages/tugas', {
+        nama: nama,
+        nim: nim,
+        alamat: alamat,
+        email: email,
+        prodi: prodi,
+        })
+    }
+
+    // Jika Tidak Error
+    if(!errors){
+        let formData = {
+                nama: nama,
+                nim: nim,
+                alamat: alamat,
+                email: email,
+                prodi: prodi,
+            }
+        // Query Insert
+        connection.query('INSERT INTO mahasiswa set ?',formData, function(err, result){
+            if(err){
+                // req.flash('error', err);
+
+                // render to add.ejs
+                res.render('pages/tugas', {
+                    nama: formData.nama,
+                    nim: formData.nim,
+                    alamat: formData.alamat,
+                    email: email,
+                    prodi: prodi
+                    })
+            }else{
+                // req.flash('success', 'Data Berhasil Disimpan!');
+                res.redirect('/mahasiswa');
+            }
+        })
+    }
+
+    
+})
 
 module.exports = router;
